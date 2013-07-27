a200b96490fa5ecf0e34b7691100fb7a6a6d8e56
Neal Barbaro <nealbarbaro@gmail.com>
2013-07-26 18:48:29 -0700

    stripe functional

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,17 +1,17 @@
 class SubscriptionsController < ApplicationController
-  def new
+  def new
     @user = User.find(session[:user_id])
-    @subscription = Subscription.new
-  end
+    @subscription = Subscription.new
+  end
 
 
-  def create
+  def create
     @user = User.find(session[:user_id])
-    @subscription = Subscription.new(params[:subscription])
-    if @subscription.save_with_payment
-      redirect_to edit_user_path(current_user), :notice => "Thank you for subscribing!"
-    else
-      render :new
-    end
-  end
+    @subscription = Subscription.new(params[:subscription])
+    if @subscription.save_with_payment
+      redirect_to edit_user_path(current_user), :notice => "Thank you for subscribing!"
+    else
+      render :new
+    end
+  end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,19 +1,19 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
 
-  attr_accessible :stripe_card_token, :user_id
+  attr_accessible :stripe_card_token, :user_id
 
-  attr_accessor :stripe_card_token
+  attr_accessor :stripe_card_token
 
-  def save_with_payment
-    if valid?
-      customer = Stripe::Customer.create(description: user_id, plan: "Premium", card: stripe_card_token)
-      self.stripe_card_token = customer.id
-      save!
-    end
-  rescue Stripe::InvalidRequestError => e
-    logger.error "Stripe error while creating customer: #{e.message}"
-    errors.add :base, "There was a problem with your credit card."
-    false
-  end
+  def save_with_payment
+    if valid?
+      customer = Stripe::Customer.create(description: user_id, plan: "Premium", card: stripe_card_token)
+      self.stripe_card_token = customer.id
+      save!
+    end
+  rescue Stripe::InvalidRequestError => e
+    logger.error "Stripe error while creating customer: #{e.message}"
+    errors.add :base, "There was a problem with your credit card."
+    false
+  end
 end
